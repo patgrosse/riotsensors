f2eeef42afb48dab1ee486332d1193edf0a8ff98
Patrick Grosse <patrick.grosse@uni-muenster.de>
2017-02-22 16:03:47 +0100

    Added linuxconnector module

---
+++ linuxconnector/include/rs_connector.h
@@ -0,0 +1,10 @@
+/*
+ *  riotsensors - RIOT-OS module for sensor data transfers
+ *
+ *  Copyright (C) 2017 Patrick Grosse <patrick.grosse@uni-muenster.de>
+ */
+
+#ifndef RIOTSENSORS_RS_CONNECTOR_H
+#define RIOTSENSORS_RS_CONNECTOR_H
+
+#endif //RIOTSENSORS_RS_CONNECTOR_H
